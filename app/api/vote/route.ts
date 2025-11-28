bfbf2d4cc55ffed1750face380ecb921343514ac
Divansh Singh <divanshsinghsaini123@gmail.com>
2025-11-28 23:28:00 +0530

    added edge runtime

--- app/api/vote/route.ts
+++ app/api/vote/route.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 import Mood from "@/models/mood";
 import { connectToDatabase } from "@/lib/mongoose";
-
+export const runtime = 'edge';
 export async function POST(req: Request) {
   try {
     await connectToDatabase();
