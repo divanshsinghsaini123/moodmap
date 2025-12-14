949ad759dfe96bdad3584a0298eea00b27d8ae8e
Divansh Singh <divanshsinghsaini123@gmail.com>
2025-12-14 23:39:59 +0530

    feat: add root layout with global styles, metadata, fonts, and Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,6 +88,7 @@ export default function RootLayout({
     <>
       <html lang="en">
         <head>
+
           <meta charSet="utf-8" />
           <meta name="google-site-verification" content="r1y8hjqObCy7zAr_dNzdzu1My1yPpVY04WtAlJEzHxg" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
